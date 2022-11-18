eb892a9021a6c67c771b68b4b840f18893901d59
Ahmed El-Zahaby <ahmedelzahaby8122000@gmail.com>
2022-11-18 14:34:41 +0200

    add three functions

--- functions.py
+++ functions.py
@@ -3,45 +3,6 @@ from Branch import Branch
 from typing import List
 from DSU import DSU
 
-def find_CL(AT, AL):
-    AT_Inverse = np.linalg.inv(AT)
-    CL = np.dot(AT_Inverse , AL)
-    return CL
-
-
-def find_BT(CL):
-    BT = -1 * np.matrix.transpose(CL)
-
-
-def find_B(BT):
-    BT_Matrix = np.matrix(BT)
-    rows,columns = BT_Matrix.shape
-    Identity_Matrix = np.eye(rows)
-    Buff = []
-    for i in range(rows):
-        Buff.append(list(np.concatenate((BT[i] , Identity_Matrix[i]))))
-    B = np.matrix(Buff)
-    return B
-
-def find_IL(B , ZB , IB , EB , BT):
-    #first term = B * ZB
-    first_term = np.dot(B , ZB)
-    #second term = B * ZB * IB
-    second_term = np.dot(first_term , IB)
-    #third term = B * EB
-    EB_matrix = np.rot90(np.rot90(np.rot90(np.matrix(EB))))
-    third_term = np.dot(B , EB_matrix)
-    divisor = np.linalg.inv(np.dot(first_term , BT))
-    IL = np.matmul(divisor,third_term)
-    return IL
-
-def find_JB(BT , IL):
-    JB = np.dot(BT , IL)
-    return  JB
-
-def find_VB(ZB , JB):
-    VB = np.dot(ZB , JB)
-    return VB
 def buildTree(N, branches):
     #Generates AT and AL
     AT= [[] for i in range(N)]
@@ -72,6 +33,85 @@ def buildTree(N, branches):
     AT.pop()
     AL.pop()
     return [AT, AL]
+
+def find_IB(branches):
+    Buff = []
+    for i in branches:
+        Buff.append(i.I)
+    IB = np.rot90(np.rot90(np.rot90(np.matrix(Buff))))
+    return IB
+
+
+def find_EB(branches):
+    Buff = []
+    for i in branches:
+        Buff.append(i.V)
+    EB = np.rot90(np.rot90(np.rot90(np.matrix(Buff))))
+    return EB
+
+
+def find_ZB(branches):
+    ZB = np.zeros((len(branches) , len(branches)))
+    for i in range(len(branches)):
+        ZB[i][i] = branches[i].R
+    return ZB
+
+
+def find_CL(AT, AL):
+    AT_Inverse = np.linalg.inv(AT)
+    CL = np.dot(AT_Inverse , AL)
+    return CL
+
+
+def find_BT(CL):
+    BT = -1 * np.matrix.transpose(CL)
+    return BT
+
+
+def find_B(BT):
+    BT_Matrix = np.matrix(BT)
+    rows,columns = BT_Matrix.shape
+    Identity_Matrix = np.eye(rows)
+    Buff = []
+    for i in range(rows):
+        Buff.append(list(np.concatenate((BT[i] , Identity_Matrix[i]))))
+    B = np.matrix(Buff)
+    return B
+
+def find_BTranspose(B):
+    B_Transpose = np.matrix.transpose(B)
+    return B_Transpose
+
+def find_IL(N , branches):
+    AT,AL = buildTree(N, branches)
+    CL = find_CL(AT , AL)
+    BTree = find_BT(CL)
+    B = find_B(BTree)
+    BT = find_BTranspose(B)
+    ZB = find_ZB(branches)
+    IB = find_IB(branches)
+    EB = find_EB(branches)
+    #first term = B * ZB
+    first_term = np.dot(B , ZB)
+    #second term = B * ZB * IB
+    second_term = np.dot(first_term , IB)
+    #third term = B * EB
+    third_term = np.dot(B , EB)
+    # return first_term
+    divisor = np.linalg.inv(np.dot(first_term , BT))
+    IL = np.matmul(divisor,third_term)
+    return [IL,BT]
+
+def find_JB(N , brances):
+    IL,BT = find_IL(N , brances)
+    JB = np.dot(BT , IL)
+    return  JB
+
+def find_VB(N , branches):
+    JB = find_JB(N , branches)
+    ZB = find_ZB(branches)
+    VB = np.dot(ZB , JB)
+    return VB
 
 if __name__ == '__main__':
     pass
--- tests.py
+++ tests.py
@@ -31,9 +31,24 @@ def test2():
           [0 , 0 , 0 , 5 , 0 , 0],
           [0 , 0 , 0 , 0 , 5 , 0],
           [0 , 0 , 0 , 0 , 0 , 10]]
-    EB = [50,0,0 , 0 , 0 , 0]
+    EB = [50, 0 , 0 , 0 , 0 , 0]
     print(find_VB(ZB , find_JB(BT , find_IL(B , ZB , 0 , EB , BT))))
 
+def test3():
+    N = 4
+    branches= []
 
-test1()
-test2()
+    branches.append(Branch(2, 0, 50, 0, 5))
+    branches.append(Branch(0, 3, 0, 0, 10))
+    branches.append(Branch(3, 2, 0, 0, 5))
+    branches.append(Branch(0, 1, 0, 0, 5))
+    branches.append(Branch(1, 2, 0, 0, 10))
+    branches.append(Branch(1, 3, 0, 0, 5))
+    print(find_JB(N , branches))
+    print("----------------------------------")
+    print(find_VB(N , branches))
+    pass
+
+
+# test1()
+test3()
